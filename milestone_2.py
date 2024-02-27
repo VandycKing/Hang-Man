485a02ccec0b7453561cc7a34fdea1cf3b847612
VandycKing <mygitglobal@gmail.com>
2024-02-27 20:06:26 +0000

    Prints elements of the list

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,13 @@
+# %%
 import random
 
+# %%
 # Creates a word list
 word_list = ["apple", "mango", "orange", "banana", "grape"]
+
+# %%
+# prints each word and its length side by side
+for word in word_list:
+    print(f"{word}:{len(word)}")  # Prints each
+
+# %%
